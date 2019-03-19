7cb162844a6f2dc55d8db4f779294d148cc20f4c
wangjichao <wangjichao@xin.com>
2019-03-19 19:21:00 +0800

    del eol

--- application/Controller/ApiController.php
+++ application/Controller/ApiController.php
@@ -20,7 +20,6 @@ class ApiController extends Controller
      */
     public function PostTask()
     {
-
         if (!ApiValidate::getInstance()->PostTaskValidate($this->data)) {
             return ApiValidate::getInstance()->getError()->__toString();
         }
