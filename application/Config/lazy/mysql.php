568e612f896a8258eac1fc49986d93734e20028f
wangjichao <wangjichao@xin.com>
2019-03-19 15:41:16 +0800

    update config

--- application/Config/lazy/mysql.php
+++ application/Config/lazy/mysql.php
@@ -12,8 +12,8 @@ return [
                 'host' => '127.0.0.1',   //数据库ip
                 'port' => 3306,          //数据库端口
                 'user' => 'root',        //数据库用户名
-                'password' => 'brave', //数据库密码
-                'database' => 'xin_dtq',   //默认数据库名
+                'password' => 'test', //数据库密码
+                'database' => 'test',   //默认数据库名
                 'timeout' => 100,       //数据库连接超时时间
                 'charset' => 'utf8mb4', //默认字符集
                 'strict_type' => true,  //ture，会自动表数字转为int类型
@@ -23,8 +23,8 @@ return [
                     'host' => '127.0.0.1',   //数据库ip
                     'port' => 3306,          //数据库端口
                     'user' => 'root',        //数据库用户名
-                    'password' => 'brave', //数据库密码
-                    'database' => 'xin_dtq',   //默认数据库名
+                    'password' => 'test', //数据库密码
+                    'database' => 'test',   //默认数据库名
                     'timeout' => 5,       //数据库连接超时时间
                     'charset' => 'utf8mb4', //默认字符集
                     'strict_type' => true,  //ture，会自动表数字转为int类型
@@ -33,8 +33,8 @@ return [
                     'host' => '127.0.0.1',   //数据库ip
                     'port' => 3306,          //数据库端口
                     'user' => 'root',        //数据库用户名
-                    'password' => 'brave', //数据库密码
-                    'database' => 'xin_dtq',   //默认数据库名
+                    'password' => 'test', //数据库密码
+                    'database' => 'test',   //默认数据库名
                     'timeout' => 5,       //数据库连接超时时间
                     'charset' => 'utf8mb4', //默认字符集
                     'strict_type' => true,  //ture，会自动表数字转为int类型
