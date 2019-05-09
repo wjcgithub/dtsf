481c7f0f45157d63cebcfb50ef5999541cfdcc8c
wangjichao <wangjichao@xin.com>
2019-05-09 20:07:01 +0800

    添加docker支持

--- application/index.php
+++ application/index.php
@@ -1,3 +1,9 @@
 <?php
+
+//$s = '123abs784bn980ajss';
+//$arr = [];
+//preg_match_all("/^[0-9].*[a-z]$/", $s, $arr);
+//print_r($arr); die;
+
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "/vendor/autoload.php";
 Dtsf\Dtsf::run();
