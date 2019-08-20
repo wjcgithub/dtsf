46fab39a3100c5f634c6684ba42a3624e2349315
evolution <wjc163@sina.cn>
2019-08-20 11:16:33 +0800

    delete test

--- application/index.php
+++ application/index.php
@@ -1,9 +1,3 @@
 <?php
-
-//$s = '123abs784bn980ajss';
-//$arr = [];
-//preg_match_all("/^[0-9].*[a-z]$/", $s, $arr);
-//print_r($arr); die;
-
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "/vendor/autoload.php";
 Dtsf\Dtsf::run();
